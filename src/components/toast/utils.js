276871ed62eef1470f10c9cb68748deba338c4e5
Raman Martynau <rmartynau@gmail.com>
2025-08-05 20:06:27 +0300

    fixed toast template

--- src/components/toast/utils.js
+++ src/components/toast/utils.js
@@ -2,10 +2,10 @@ import { TOAST_TYPE } from "../../constants/toast";
 
 export const getToastType = (type) => {
   const types = {
-    [TOAST_TYPE.error]: "border-t-red-500",
-    [TOAST_TYPE.info]: "border-t-blue-500",
-    [TOAST_TYPE.warning]: "border-t-amber-500",
-    [TOAST_TYPE.success]: "border-t-green-500",
+    [TOAST_TYPE.error]: "border-red-500",
+    [TOAST_TYPE.info]: "border-blue-500",
+    [TOAST_TYPE.warning]: "border-yellow-500",
+    [TOAST_TYPE.success]: "border-green-500",
   };
 
   return types[type];
